d28c07e7c69703d25561618e193529e4f9ef7798
Ahmed Elhady <ahmedelhady0321@gmail.com>
2025-08-25 16:26:29 +0300

    Added common HTTP middlewares including security and logging requests

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,9 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 // routes returns a *http.ServeMux that contains our all routes.
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -14,5 +18,7 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	return mux
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
+	return standard.Then(mux)
 }
